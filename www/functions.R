dd00069e15cf6d1b5f992dcbfa1b75610d1d4497
jeremiedb <nimus44@gmail.com>
2016-05-02 04:23:53 -0400

    Functions for model scoring

---
+++ www/functions.R
@@ -0,0 +1,24 @@
+SumModelGini <- function(solution, submission) {
+  df = data.frame(solution = solution, submission = submission)
+  df <- df[order(df$submission, decreasing = TRUE),]
+  df$random = (1:nrow(df))/nrow(df)
+  totalPos <- sum(df$solution)
+  df$cumPosFound <- cumsum(df$solution) # this will store the cumulative number of positive examples found (used for computing "Model Lorentz")
+  df$Lorentz <- df$cumPosFound / totalPos # this will store the cumulative proportion of positive examples found ("Model Lorentz")
+  df$Gini <- df$Lorentz - df$random # will store Lorentz minus random
+  return(sum(df$Gini))
+}
+
+NormalizedGini <- function(solution, submission) {
+  SumModelGini(solution, submission) / SumModelGini(solution, solution)
+}
+
+### Root Mean Square Percentage Error
+RMSPE <- function(solution, submission) {
+  sqrt(mean((submission/solution-1)^2))
+}
+
+### Root Mean Square Percentage Error
+LogLoss <- function(solution, submission) {
+  -mean(solution*log(submission)+(1-solution)*log(1-submission))
+}
